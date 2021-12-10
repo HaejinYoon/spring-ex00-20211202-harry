ae30ad1e62502dceb03614ff22d575e1451853e9
Haejin Yoon <hjyoomp@gmail.com>
2021-12-10 14:06:58 +0900

    211210 1st lecture commit

---
+++ src/main/java/org/zerock/controller/project1/BoardController.java
@@ -0,0 +1,56 @@
+package org.zerock.controller.project1;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.project1.BoardVO;
+import org.zerock.service.project1.BoardService;
+
+import lombok.Setter;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		// 3. business logic
+		// 게시물(Board) 목록 조회
+		List<BoardVO> list = service.getList();
+		
+		// 4.
+		model.addAttribute("list", list);
+		
+		// 5.
+		// jsp path : /WEB-INF/views/board/list.jsp
+	}
+	// /board/get?id=10
+	@GetMapping("/get")
+	public void get(@RequestParam("id") Integer id, Model model) {
+		BoardVO board = service.get(id);
+		
+		model.addAttribute("board", board);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
+++ src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
@@ -3,6 +3,10 @@ package org.zerock.mapper.project1;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -80,10 +84,55 @@ public class BoardMapperTest {
 		
 		assertEquals(newTitle, updatedVO.getTitle());
 		assertEquals(newContent, updatedVO.getContent());
-		
-		
 	}
 	
+	@Test
+	public void insertAndDeleteTest() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("delete Title");
+		vo.setContent("delete Content");
+		vo.setWriter("deleter");
+		
+		int cnt = mapper.insert(vo);
+		
+		assertEquals(1, cnt);
+		
+		cnt = mapper.delete(vo.getId());
+		
+		assertEquals(1, cnt);
+		
+		BoardVO deleted = mapper.read(vo.getId());
+		assertNull(deleted);
+	}
+	
+	@Test
+	public void getListTest() {
+		List<BoardVO> list = mapper.getList();
+		assertNotNull(list);
+		assertTrue(list.size()>0);
+		
+		BoardVO vo = new BoardVO();
+		vo.setTitle("list Test");
+		vo.setContent("list Test Content");
+		vo.setWriter("tester");
+		
+		mapper.insert(vo);
+		
+		List<BoardVO> list2 = mapper.getList();
+		
+		assertEquals(list.size()+1, list2.size());
+		
+		for(BoardVO item : list2) {
+			assertNotNull(item.getId());
+			assertNotNull(item.getTitle());
+			assertNotNull(item.getContent());
+			assertNotNull(item.getWriter());
+			assertNotNull(item.getInserted());
+			assertNotNull(item.getUpdated());
+			
+		}
+		
+	}
 	
 	
 	
