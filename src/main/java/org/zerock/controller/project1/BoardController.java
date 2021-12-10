05d6bec4a1d5cafd96795920e31d279a9604d6a6
Haejin Yoon <hjyoomp@gmail.com>
2021-12-10 15:09:18 +0900

    Update BoardController.java

--- src/main/java/org/zerock/controller/project1/BoardController.java
+++ src/main/java/org/zerock/controller/project1/BoardController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.project1.BoardVO;
 import org.zerock.service.project1.BoardService;
 
@@ -39,7 +41,30 @@ public class BoardController {
 		
 		model.addAttribute("board", board);
 	}
+	@GetMapping("/register")
+	public void register() {
+		
+	}
 	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		// 2. request 분석 가공 BoardVO board 명시하는 것으로 생략가능
+		// 3. 
+		service.register(board);
+		// 4. add attribute
+		rttr.addAttribute("result", board.getId());
+		// 5. forward/ redirect
+		// 책 : 목록으로 redirect
+		
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/modify")
+	public void modify(@RequestParam("id") Integer id, Model model ) {
+		BoardVO board = service.get(id);
+		
+		model.addAttribute("board", board);
+	}
 }
 
 
