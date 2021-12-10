3555c4503e0e9af91bc098d6e4893c0497ca3a41
Haejin Yoon <hjyoomp@gmail.com>
2021-12-10 10:12:44 +0900

    Update BoardMapperTest.java

--- src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
+++ src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
@@ -1,6 +1,7 @@
 package org.zerock.mapper.project1;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -22,6 +23,7 @@ public class BoardMapperTest {
 	public void mapperTest() {
 		assertNotNull(mapper);
 	}
+	
 	@Test
 	public void insertTest() {
 		BoardVO vo = new BoardVO();
@@ -32,7 +34,64 @@ public class BoardMapperTest {
 		int cnt = mapper.insert(vo);
 		
 		assertEquals(1, cnt);
+	}
+	
+	@Test
+	public void insertAndSelectTest() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("Test title" + Math.random());
+		vo.setContent("Test content" + Math.random());
+		vo.setWriter("test writer" + Math.random());
+		
+		mapper.insert(vo);
+		
+		assertNotNull(vo.getId());
+		assertNotEquals(0,  vo.getId().intValue());
+		
+		BoardVO lastInserted = mapper.read(vo.getId());
+		
+		assertEquals(vo.getTitle(), lastInserted.getTitle());
+		assertEquals(vo.getContent(), lastInserted.getContent());
+		assertEquals(vo.getWriter(), lastInserted.getWriter());
 		
 	}
 
+	@Test
+	public void updateTest() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("Test title" + Math.random());
+		vo.setContent("Test content" + Math.random());
+		vo.setWriter("test writer" + Math.random());
+		
+		mapper.insert(vo);
+		
+		assertNotNull(vo.getId());
+		assertNotEquals(0,  vo.getId().intValue());
+		
+		String newTitle="update title" + Math.random();
+		String newContent = "update contenet" + Math.random();
+		
+		vo.setTitle(newTitle);
+		vo.setContent(newContent);
+		
+		mapper.update(vo);
+		
+		BoardVO updatedVO = mapper.read(vo.getId());
+		
+		assertEquals(newTitle, updatedVO.getTitle());
+		assertEquals(newContent, updatedVO.getContent());
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
