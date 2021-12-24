068a5b2f70d07942753ccc1debaeaa6f684d17d2
Haejin Yoon <hjyoomp@gmail.com>
2021-12-25 07:14:47 +0900

    Update 01board.sql

--- src/main/webapp/WEB-INF/sql/01board.sql
+++ src/main/webapp/WEB-INF/sql/01board.sql
@@ -8,7 +8,7 @@ CREATE TABLE Board(
     inserted DATETIME DEFAULT NOW(),
     updated DATETIME DEFAULT NOW()
 );
-select * from Board;
+select * from Board ORDER BY id DESC;
 DESC Board;
 
 DELETE FROM Board WHERE
@@ -21,7 +21,9 @@ ALTER TABLE Board MODIFY COLUMN inserted DATETIME NOT NULL DEFAULT NOW();
 ALTER TABLE Board MODIFY COLUMN updated DATETIME NOT NULL DEFAULT NOW();
 
 ALTER TABLE Board MODIFY COLUMN views INT NOT NULL DEFAULT 0;
-UPDATE Board set views = 0;
+ALTER TABLE Board ADD COLUMN notice INT DEFAULT 0;
+
+UPDATE Board set notice = 0 ;
 
 UPDATE 
 		Board
@@ -43,6 +45,7 @@ INSERT INTO Board (title, content, writer)
     	b.inserted,
     	b.updated,
     	b.views,
+        b.notice,
     	m.nickName,
         count(r.id) replyCount
 	FROM
@@ -60,3 +63,26 @@ INSERT INTO Board (title, content, writer)
     -- page 2 : 10, 10
     -- page 3 : 20, 10
     -- page 4 : 30, 10
+    
+SELECT 
+    	b.id,
+    	b.title,
+    	b.content,
+    	b.writer,
+    	b.inserted,
+    	b.updated,
+    	b.views,
+    	b.notice,
+    	m.nickName,
+    	COUNT(r.id) replyCount,
+    	(SELECT EXISTS (SELECT * FROM File WHERE boardId = b.id)) hasFile,
+    	datediff(now(), b.inserted) newMark
+	FROM
+    	Board b
+    JOIN
+    	Member m ON b.writer = m.id
+    LEFT JOIN
+    	Reply r ON b.id = r.boardId
+	WHERE title like concat('%',test,'%' )
+	GROUP BY b.id
+    ORDER BY inserted DESC;
